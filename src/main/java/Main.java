9f540a1c1de5b5ab52fb9fdb1a18f5e17f9179d0
abdo <abdelsalem.yabrag@hotmail.com>
2020-05-12 19:06:09 +0200

    Finished layout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,21 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main  extends Application {
 
@@ -18,30 +23,79 @@ public class Main  extends Application {
     public void start(Stage primaryStage) throws Exception {
         VBox mainContainer = new VBox();
 
-        HBox topContainer = new HBox();
-        topContainer.setMinHeight(200);
-        topContainer.setMinWidth(600);
-        topContainer.setBackground(new Background(new BackgroundFill(Color.AQUA, null, null)));
+        mainContainer.getChildren().addAll(getTopContainer(), getBottomContainer());
 
-        HBox bottomContainer = new HBox();
-        bottomContainer.setMinHeight(400);
-        bottomContainer.setMinWidth(600);
-        bottomContainer.setBackground(new Background(new BackgroundFill(Color.BISQUE, null, null)));
-
-        Button button; // Skapa 5 stycken
-        RadioButton radioButton; // Skapa 2 av den här och lägga dem i vbox
-        ButtonGroup buttonGroup = new ButtonGroup(); // Lägg radio buttons i den här
-
-        TextField textField;
-
-        /*
-        Lägga alla knappar och textfältet till topContainer
-         */
-
-        mainContainer.getChildren().addAll(topContainer, bottomContainer);
-
-        Scene scene = new Scene(mainContainer, 600, 800);
+        Scene scene = new Scene(mainContainer, 1100, 800);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private HBox getTopContainer() {
+        HBox topContainer = new HBox();
+        topContainer.setPadding(new Insets(14, 16, 14, 16));
+        topContainer.setSpacing(12);
+
+        Button newButton = new Button("New"); // Skapa 5 stycken
+        Button searchButton = new Button("Search");
+        Button removeButton = new Button("Remove");
+        Button hideButton = new Button("Hide");
+        Button coordinatesButton = new Button("Coordinates");
+
+        TextField textField = new TextField();
+        topContainer.getChildren().addAll(newButton, getRadioBox(), textField,
+                searchButton, removeButton, hideButton, coordinatesButton);
+
+        return topContainer;
+    }
+
+    private VBox getRadioBox() {
+        RadioButton namedRadio = new RadioButton("Named"); // Skapa 2 av den här och lägga dem i vbox
+        RadioButton described = new RadioButton("Described"); // Skapa 2 av den här och lägga dem i vbox
+        VBox radioBox = new VBox();
+        radioBox.getChildren().addAll(namedRadio, described);
+        radioBox.setSpacing(6);
+        ToggleGroup toggleGroup = new ToggleGroup(); // Lägg radio buttons i den här
+        namedRadio.setToggleGroup(toggleGroup);
+        described.setToggleGroup(toggleGroup);
+
+        return radioBox;
+    }
+
+    private HBox getBottomContainer() {
+        HBox bottomContainer = new HBox();
+        StackPane mapStack = new StackPane();
+        ImageView imageView = new ImageView();
+
+        try {
+            InputStream inputStream = new FileInputStream("C:/AwesomeMaps/exempelkarta.png");
+            imageView.setImage(new Image(inputStream));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        mapStack.getChildren().addAll(imageView);
+        bottomContainer.getChildren().addAll(mapStack, getRightPanel());
+        return bottomContainer;
+    }
+
+    private VBox getRightPanel() {
+        VBox rightPanel = new VBox();
+        Label categoriesLabel = new Label("Categories");
+
+        ListView<String> categoriesListView = new ListView<>();
+        List<String> categories = new ArrayList<>();
+        categories.add("Train");
+        categories.add("Bus");
+        categories.add("Underground");
+        categoriesListView.setItems(FXCollections.observableList(categories));
+
+        Button hideCatButton = new Button("Hide Categories");
+        rightPanel.setSpacing(12);
+        rightPanel.setPadding(new Insets(14, 16, 14, 16));
+
+        rightPanel.getChildren().addAll(categoriesLabel, categoriesListView, hideCatButton);
+
+        return rightPanel;
     }
 }
